742501465e6f2983f7259c35957f7a4314f48ff8
John Paul Ada <johnpaulada@gmail.com>
2018-07-26 00:56:38 +0800

    Added stuff

--- src/App.js
+++ src/App.js
@@ -1,66 +1,23 @@
 import React, { Component } from 'react';
-import Camera from 'react-camera';
+import Cam from './Cam';
+import {
+    AppContainer,
+    EmojiContainer
+} from './Styles'
 
-export default class App extends Component {
+class App extends Component {
+    render() {
+        return <AppContainer>
+            <EmojiContainer>
+                <button onClick={this.train}>Train</button>
+            </EmojiContainer>
+            <Cam />
+        </AppContainer>
+    }
 
-  constructor(props) {
-    super(props);
-    this.takePicture = this.takePicture.bind(this);
-  }
-
-  takePicture() {
-    this.camera.capture()
-    .then(blob => {
-      this.img.src = URL.createObjectURL(blob);
-      this.img.onload = () => { URL.revokeObjectURL(this.src); }
-    })
-  }
-
-  render() {
-    return (
-      <div style={style.container}>
-        <Camera
-          style={style.preview}
-          ref={(cam) => {
-            this.camera = cam;
-          }}
-        >
-          <div style={style.captureContainer} onClick={this.takePicture}>
-            <div style={style.captureButton} />
-          </div>
-        </Camera>
-        <img
-          style={style.captureImage}
-          ref={(img) => {
-            this.img = img;
-          }}
-        />
-      </div>
-    );
-  }
+    train = () => {
+        
+    }
 }
 
-const style = {
-  preview: {
-    position: 'relative',
-  },
-  captureContainer: {
-    display: 'flex',
-    position: 'absolute',
-    justifyContent: 'center',
-    zIndex: 1,
-    bottom: 0,
-    width: '100%'
-  },
-  captureButton: {
-    backgroundColor: '#fff',
-    borderRadius: '50%',
-    height: 56,
-    width: 56,
-    color: '#000',
-    margin: 20
-  },
-  captureImage: {
-    width: '100%',
-  }
-};
+export default App
---
+++ src/Styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+`
+
+export const EmojiContainer = styled.div`
+    width: 100vw;
+    height: 50vh;
+`
